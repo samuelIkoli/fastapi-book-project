edc099ca729980f62bf8dc7d27016558ee738078
samuelIkoli <ayibanimiikoli@gmail.com>
2025-02-11 15:50:44 +0100

    ??

--- api/routes/books.py
+++ api/routes/books.py
@@ -25,7 +25,7 @@ db.books = {
     ),
     3: Book(
         id=3,
-        title="The Return of the Kingolo",
+        title="The Return of the Kingers",
         author="J.R.R. Tolkien",
         publication_year=1955,
         genre=Genre.FANTASY,
